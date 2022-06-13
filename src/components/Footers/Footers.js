f3bf8bae37acae9c42185eabe8169f86c5461259
Dennis <officialweru@gmail.com>
2022-06-13 18:49:43 +0300

    added login and register

---
+++ src/components/Footers/Footers.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Footers.module.css';
+
+const Footers = () => (
+  <div className={styles.Footers} data-testid="Footers">
+
+    <footer class="bg-secondary pt-3">
+      <div class="text-center pb-3">
+        <p class="mb-0">Copyright ©
+          <script>var CurrentYear = new Date().getFullYear()
+            document.write(CurrentYear)</script> <br /> A project by <a href="https://dennisweru.co.ke/">Dennis
+              Weru</a>
+        </p>
+      </div>
+    </footer>
+  </div>
+);
+
+Footers.propTypes = {};
+
+Footers.defaultProps = {};
+
+export default Footers;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/register" element={<Register />} />
+      <Route exact path="/" element={<Login />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/home" element={<Home />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
